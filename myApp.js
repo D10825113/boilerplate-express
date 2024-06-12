2044268599d714dafddca856d7699eb3e04ea340
Chad <a089831918@gmail.com>
2024-06-12 22:59:48 +0800

    使用 .env 文件

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+
 
 
 
@@ -10,9 +12,14 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
-});
+  let message = "Hello json";
 
+  if (process.env.MESSAGE_STYLE==="uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message });
+});
 
 
 
